c29cf67c44e92dab1709ace48b55a65e5b12eee1
IVAN A. LAVAYEN <ivanalejolavayen@gmail.com>
2023-10-22 20:02:49 -0300

    armado del casillero con la imagen

--- Casillero.js
+++ Casillero.js
@@ -1,26 +1,32 @@
 class Casillero {
-	constructor(imagen, ancho, alto, contexto, posX, posY) {
-		this.imagen = imagen;
-		this.ancho = ancho;
-		this.alto = alto;
-		this.contexto = contexto;
-		this.posX = posX;
-		this.posY = posY;
-	}
+    constructor(imagen, ancho, alto, contexto, posX, posY) {
+        this.imagen = new Image();
+        this.imagen.src = "casillero.jpg"; // Asigna la ruta de la imagen al objeto Image
+        this.ancho = ancho;
+        this.alto = alto;
+        this.contexto = contexto;
+        this.posX = posX;
+        this.posY = posY;
+        this.imagenCargada = false; // Bandera para verificar si la imagen se ha cargado
+        this.imagen.onload = () => {
+            this.imagenCargada = true;
+            this.dibujar();
+        };
+    }
 
-	getAncho() {
-		return this.ancho;
-	}
+    getAncho() {
+        return this.ancho;
+    }
 
-	getAlto() {
+    getAlto() {
         return this.alto;
     }
 
-	dibujar() {
-		this.contexto.fillStyle = "#FFAABB";
-		this.contexto.fillRect(this.posX, this.posY, this.ancho, this.alto);
-        this.contexto.strokeStyle = "black";
-        this.contexto.strokeRect(this.posX, this.posY, this.ancho, this.alto);
-        // this.contexto.drawImage(this.imagen, this.posX, this.posY);
-	}
+    dibujar() {
+        if (!this.imagenCargada) {
+            return; // No  dibuja la imagen si no se ha cargado
+        }
+             
+        this.contexto.drawImage(this.imagen, this.posX, this.posY, this.ancho, this.alto);
+    }
 }
